d0862e2f05133c1519cf648e99d74f5ffbb9eee8
Wenzhang Qian <wenzhangq@gmail.com>
2018-04-26 14:21:23 -0600

    src added

--- src/main.cpp
+++ src/main.cpp
@@ -3,24 +3,24 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
-#include <iomanip>
-#include <boost/asio.hpp>
-#include <boost/bind.hpp>
+//#include <iomanip>
+//#include <boost/asio.hpp>
+//#include <boost/bind.hpp>
 
-#include <OpenNI.h>
+//#include <OpenNI.h>
 
 #include "kcftracker.hpp"
 
 
-boost::asio::io_service iosev;
-boost::asio::serial_port sp(iosev);
+//boost::asio::io_service iosev;
+//boost::asio::serial_port sp(iosev);
 
 unsigned char startByte = 0xff;
 unsigned char endByte = 0xfe;
 std::string Base_Port = "/dev/ttyUSB0";
 
 using namespace std;
-using namespace openni;
+//using namespace openni;
 using namespace cv;
 
 static const std::string RGB_WINDOW = "RGB Image window";
@@ -78,29 +78,29 @@ union Max_Value {
 } Send_Data;
 
 
-void openPort()
-{
-    sp.open(Base_Port);
-    sp.set_option(boost::asio::serial_port::baud_rate(9600));
-    sp.set_option(boost::asio::serial_port::flow_control(boost::asio::serial_port::flow_control::none));
-    sp.set_option(boost::asio::serial_port::parity(boost::asio::serial_port::parity::none));
-    sp.set_option(boost::asio::serial_port::stop_bits(boost::asio::serial_port::stop_bits::one));
-    sp.set_option(boost::asio::serial_port::character_size(8));
-}
+//void openPort()
+//{
+//    sp.open(Base_Port);
+//    sp.set_option(boost::asio::serial_port::baud_rate(9600));
+//    sp.set_option(boost::asio::serial_port::flow_control(boost::asio::serial_port::flow_control::none));
+//    sp.set_option(boost::asio::serial_port::parity(boost::asio::serial_port::parity::none));
+//    sp.set_option(boost::asio::serial_port::stop_bits(boost::asio::serial_port::stop_bits::one));
+//    sp.set_option(boost::asio::serial_port::character_size(8));
+//}
 
-/**
- * send the velocity to the lower machine
- * @param vTranslation [the translation velocity]    max <= 0.8
- * @param vRotation    [the rotation velocity]       max <= 1.0
- */
-void sendVelocity(float vTranslation, float vRotation)
-{
-    Send_Data.Double_RAM._double_vT = vTranslation;
-    Send_Data.Double_RAM._double_vR = vRotation;
-    write(sp, boost::asio::buffer(&startByte, 1));
-    write(sp, boost::asio::buffer(Send_Data.buf, 8));
-    write(sp, boost::asio::buffer(&endByte, 1));
-}
+///**
+// * send the velocity to the lower machine
+// * @param vTranslation [the translation velocity]    max <= 0.8
+// * @param vRotation    [the rotation velocity]       max <= 1.0
+// */
+//void sendVelocity(float vTranslation, float vRotation)
+//{
+//    Send_Data.Double_RAM._double_vT = vTranslation;
+//    Send_Data.Double_RAM._double_vR = vRotation;
+//    write(sp, boost::asio::buffer(&startByte, 1));
+//    write(sp, boost::asio::buffer(Send_Data.buf, 8));
+//    write(sp, boost::asio::buffer(&endByte, 1));
+//}
 
 void onMouse(int event, int x, int y, int, void*)
 {
@@ -127,24 +127,24 @@ void onMouse(int event, int x, int y, int, void*)
 }
 
 
-void showdevice(){
-    // 获取设备信息
-    Array<DeviceInfo> aDeviceList;
-    OpenNI::enumerateDevices(&aDeviceList);
-
-    cout << "电脑上连接着 " << aDeviceList.getSize() << " 个体感设备." << endl;
-
-    for (int i = 0; i < aDeviceList.getSize(); ++i)
-    {
-        cout << "设备 " << i << endl;
-        const DeviceInfo& rDevInfo = aDeviceList[i];
-        cout << "设备名： " << rDevInfo.getName() << endl;
-        cout << "设备Id： " << rDevInfo.getUsbProductId() << endl;
-        cout << "供应商名： " << rDevInfo.getVendor() << endl;
-        cout << "供应商Id: " << rDevInfo.getUsbVendorId() << endl;
-        cout << "设备URI: " << rDevInfo.getUri() << endl;
-    }
-}
+//void showdevice(){
+//    // 获取设备信息
+//    Array<DeviceInfo> aDeviceList;
+//    OpenNI::enumerateDevices(&aDeviceList);
+//
+//    cout << "电脑上连接着 " << aDeviceList.getSize() << " 个体感设备." << endl;
+//
+//    for (int i = 0; i < aDeviceList.getSize(); ++i)
+//    {
+//        cout << "设备 " << i << endl;
+//        const DeviceInfo& rDevInfo = aDeviceList[i];
+//        cout << "设备名： " << rDevInfo.getName() << endl;
+//        cout << "设备Id： " << rDevInfo.getUsbProductId() << endl;
+//        cout << "供应商名： " << rDevInfo.getVendor() << endl;
+//        cout << "供应商Id: " << rDevInfo.getUsbVendorId() << endl;
+//        cout << "设备URI: " << rDevInfo.getUri() << endl;
+//    }
+//}
 
 void hMirrorTrans(const Mat &src, Mat &dst)
 {
@@ -183,135 +183,143 @@ void hMirrorTrans(const Mat &src, Mat &dst)
 
 }
 
-int main()
+int main(string file_path)
 {
-    Status rc = STATUS_OK;
-
-    // 初始化OpenNI环境
-    OpenNI::initialize();
-
-    showdevice();
-
-    // 声明并打开Device设备。
-    Device xtion;
-    const char * deviceURL = openni::ANY_DEVICE;  //设备名
-    rc = xtion.open(deviceURL);
-
-    // 创建深度数据流
-    VideoStream streamDepth;
-    rc = streamDepth.create(xtion, SENSOR_DEPTH);
-    if (rc == STATUS_OK)
-    {
-        // 设置深度图像视频模式
-        VideoMode mModeDepth;
-        // 分辨率大小
-        mModeDepth.setResolution(640, 480);
-        // 每秒30帧
-        mModeDepth.setFps(30);
-        // 像素格式
-        mModeDepth.setPixelFormat(PIXEL_FORMAT_DEPTH_1_MM);
-
-        streamDepth.setVideoMode(mModeDepth);
-
-        // 打开深度数据流
-        rc = streamDepth.start();
-        if (rc != STATUS_OK)
-        {
-            cerr << "无法打开深度数据流：" << OpenNI::getExtendedError() << endl;
-            streamDepth.destroy();
-        }
-    }
-    else
-    {
-        cerr << "无法创建深度数据流：" << OpenNI::getExtendedError() << endl;
-    }
-
-    // 创建彩色图像数据流
-    VideoStream streamColor;
-    rc = streamColor.create(xtion, SENSOR_COLOR);
-    if (rc == STATUS_OK)
-    {
-        // 同样的设置彩色图像视频模式
-        VideoMode mModeColor;
-        mModeColor.setResolution(320, 240);
-        mModeColor.setFps(30);
-        mModeColor.setPixelFormat(PIXEL_FORMAT_RGB888);
-
-        streamColor.setVideoMode(mModeColor);
-
-        // 打开彩色图像数据流
-        rc = streamColor.start();
-        if (rc != STATUS_OK)
-        {
-            cerr << "无法打开彩色图像数据流：" << OpenNI::getExtendedError() << endl;
-            streamColor.destroy();
-        }
-    }
-    else
-    {
-        cerr << "无法创建彩色图像数据流：" << OpenNI::getExtendedError() << endl;
-    }
-
-    if (!streamColor.isValid() || !streamDepth.isValid())
-    {
-        cerr << "彩色或深度数据流不合法" << endl;
-        OpenNI::shutdown();
-        return 1;
-    }
-
-    // 图像模式注册,彩色图与深度图对齐
-    if (xtion.isImageRegistrationModeSupported(
-        IMAGE_REGISTRATION_DEPTH_TO_COLOR))
-    {
-        xtion.setImageRegistrationMode(IMAGE_REGISTRATION_DEPTH_TO_COLOR);
-    }
-
-
-    // 创建OpenCV图像窗口
-    namedWindow(RGB_WINDOW, CV_WINDOW_AUTOSIZE);
-    //namedWindow(DEPTH_WINDOW, CV_WINDOW_AUTOSIZE);
-
-    // 获得最大深度值
-    int iMaxDepth = streamDepth.getMaxPixelValue();
-
-    // 循环读取数据流信息并保存在VideoFrameRef中
-    VideoFrameRef  frameDepth;
-    VideoFrameRef  frameColor;
+    string depth_path = file_path + "/depth";
+    string color_path = file_path + "/rgb";
+//    Status rc = STATUS_OK;
+//
+//    // 初始化OpenNI环境
+//    OpenNI::initialize();
+//
+//    showdevice();
+//
+//    // 声明并打开Device设备。
+//    Device xtion;
+//    const char * deviceURL = openni::ANY_DEVICE;  //设备名
+//    rc = xtion.open(deviceURL);
+//
+//    // 创建深度数据流
+//    VideoStream streamDepth;
+//    rc = streamDepth.create(xtion, SENSOR_DEPTH);
+//    if (rc == STATUS_OK)
+//    {
+//        // 设置深度图像视频模式
+//        VideoMode mModeDepth;
+//        // 分辨率大小
+//        mModeDepth.setResolution(640, 480);
+//        // 每秒30帧
+//        mModeDepth.setFps(30);
+//        // 像素格式
+//        mModeDepth.setPixelFormat(PIXEL_FORMAT_DEPTH_1_MM);
+//
+//        streamDepth.setVideoMode(mModeDepth);
+//
+//        // 打开深度数据流
+//        rc = streamDepth.start();
+//        if (rc != STATUS_OK)
+//        {
+//            cerr << "无法打开深度数据流：" << OpenNI::getExtendedError() << endl;
+//            streamDepth.destroy();
+//        }
+//    }
+//    else
+//    {
+//        cerr << "无法创建深度数据流：" << OpenNI::getExtendedError() << endl;
+//    }
+//
+//    // 创建彩色图像数据流
+//    VideoStream streamColor;
+//    rc = streamColor.create(xtion, SENSOR_COLOR);
+//    if (rc == STATUS_OK)
+//    {
+//        // 同样的设置彩色图像视频模式
+//        VideoMode mModeColor;
+//        mModeColor.setResolution(320, 240);
+//        mModeColor.setFps(30);
+//        mModeColor.setPixelFormat(PIXEL_FORMAT_RGB888);
+//
+//        streamColor.setVideoMode(mModeColor);
+//
+//        // 打开彩色图像数据流
+//        rc = streamColor.start();
+//        if (rc != STATUS_OK)
+//        {
+//            cerr << "无法打开彩色图像数据流：" << OpenNI::getExtendedError() << endl;
+//            streamColor.destroy();
+//        }
+//    }
+//    else
+//    {
+//        cerr << "无法创建彩色图像数据流：" << OpenNI::getExtendedError() << endl;
+//    }
+//
+//    if (!streamColor.isValid() || !streamDepth.isValid())
+//    {
+//        cerr << "彩色或深度数据流不合法" << endl;
+//        OpenNI::shutdown();
+//        return 1;
+//    }
+//
+//    // 图像模式注册,彩色图与深度图对齐
+//    if (xtion.isImageRegistrationModeSupported(
+//        IMAGE_REGISTRATION_DEPTH_TO_COLOR))
+//    {
+//        xtion.setImageRegistrationMode(IMAGE_REGISTRATION_DEPTH_TO_COLOR);
+//    }
+//
+//
+//    // 创建OpenCV图像窗口
+//    namedWindow(RGB_WINDOW, CV_WINDOW_AUTOSIZE);
+//    //namedWindow(DEPTH_WINDOW, CV_WINDOW_AUTOSIZE);
+//
+//    // 获得最大深度值
+//    int iMaxDepth = streamDepth.getMaxPixelValue();
+//
+//    // 循环读取数据流信息并保存在VideoFrameRef中
+//    VideoFrameRef  frameDepth;
+//    VideoFrameRef  frameColor;
 
     //openPort();
 
     while (true)
     {
-        // 读取数据流
-        rc = streamDepth.readFrame(&frameDepth);
-        if (rc == STATUS_OK)
-        {
-            // 将深度数据转换成OpenCV格式
-            const Mat mImageDepth(frameDepth.getHeight(), frameDepth.getWidth(), CV_16UC1, (void*)frameDepth.getData());
-            // 为了让深度图像显示的更加明显一些，将CV_16UC1 ==> CV_8U格式
-            //Mat mScaledDepth;//, hScaledDepth;
-            //mImageDepth.convertTo(mScaledDepth, CV_8U, 255.0 / iMaxDepth);
-            // cout << " 1 " << mImageDepth.ptr<ushort>(320)[240] << endl;
-            // cout << " 2 " << mImageDepth.at<float>(320,240) << endl;
-            //水平镜像深度图
-            //hMirrorTrans(mScaledDepth, depthimage);
-            //hMirrorTrans(mImageDepth, depthimage);
-            mImageDepth.copyTo(depthimage);
-        }
+        // read color image
+        Mat image;
+        image = imread(argv[1], CV_LOAD_IMAGE_COLOR);   // Read the file
+        // read depth image
 
-        rc = streamColor.readFrame(&frameColor);
-        if (rc == STATUS_OK)
-        {
-            // 同样的将彩色图像数据转化成OpenCV格式
-            const Mat mImageRGB(frameColor.getHeight(), frameColor.getWidth(), CV_8UC3, (void*)frameColor.getData());
-            // 首先将RGB格式转换为BGR格式
-            // Mat cImageBGR,bImageBGR;//,hImageBGR;
-            cvtColor(mImageRGB, rgbimage, CV_RGB2BGR);
 
-            //水平镜像深度图
-            //hMirrorTrans(cImageBGR, rgbimage);
-            resize(rgbimage, rgbimage, Size(640, 480));     
-        }
+//        // 读取数据流
+//        rc = streamDepth.readFrame(&frameDepth);
+//        if (rc == STATUS_OK)
+//        {
+//            // 将深度数据转换成OpenCV格式
+//            const Mat mImageDepth(frameDepth.getHeight(), frameDepth.getWidth(), CV_16UC1, (void*)frameDepth.getData());
+//            // 为了让深度图像显示的更加明显一些，将CV_16UC1 ==> CV_8U格式
+//            //Mat mScaledDepth;//, hScaledDepth;
+//            //mImageDepth.convertTo(mScaledDepth, CV_8U, 255.0 / iMaxDepth);
+//            // cout << " 1 " << mImageDepth.ptr<ushort>(320)[240] << endl;
+//            // cout << " 2 " << mImageDepth.at<float>(320,240) << endl;
+//            //水平镜像深度图
+//            //hMirrorTrans(mScaledDepth, depthimage);
+//            //hMirrorTrans(mImageDepth, depthimage);
+//            mImageDepth.copyTo(depthimage);
+//        }
+//
+//        rc = streamColor.readFrame(&frameColor);
+//        if (rc == STATUS_OK)
+//        {
+//            // 同样的将彩色图像数据转化成OpenCV格式
+//            const Mat mImageRGB(frameColor.getHeight(), frameColor.getWidth(), CV_8UC3, (void*)frameColor.getData());
+//            // 首先将RGB格式转换为BGR格式
+//            // Mat cImageBGR,bImageBGR;//,hImageBGR;
+//            cvtColor(mImageRGB, rgbimage, CV_RGB2BGR);
+//
+//            //水平镜像深度图
+//            //hMirrorTrans(cImageBGR, rgbimage);
+//            resize(rgbimage, rgbimage, Size(640, 480));
+//        }
 
         setMouseCallback(RGB_WINDOW, onMouse, 0);
 
@@ -404,13 +412,13 @@ int main()
         }
     }
     destroyWindow(RGB_WINDOW);
-    // 关闭数据流
-    streamDepth.destroy();
-    streamColor.destroy();
-    // 关闭设备
-    xtion.close();
-    // 最后关闭OpenNI
-    OpenNI::shutdown();
+//    // 关闭数据流
+//    streamDepth.destroy();
+//    streamColor.destroy();
+//    // 关闭设备
+//    xtion.close();
+//    // 最后关闭OpenNI
+//    OpenNI::shutdown();
 
     return 0;
 }
