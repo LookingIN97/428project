dbe845c32b0b91bcfcb4f6b6bbe5a6b01a79d01c
Wenzhang Qian <wenzhangq@gmail.com>
2018-04-25 19:01:59 -0600

    copied

---
+++ cmake-build-debug/CMakeFiles/my.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my.dir/main.cpp.o"
+  "my.pdb"
+  "my"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
